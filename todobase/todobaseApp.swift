24353e8d40bad21d1c7a782896ec6c7bba7a7d85
Prince Mahmud <46414838+devprinxe@users.noreply.github.com>
2026-02-15 09:53:55 +0600

    Initial Commit

---
+++ todobase/todobaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  todobaseApp.swift
+//  todobase
+//
+//  Created by TechnoNext on 15/2/26.
+//
+
+import SwiftUI
+
+@main
+struct todobaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
